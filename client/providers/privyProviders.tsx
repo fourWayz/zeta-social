a19c7afde1eba8edd1e489d5ed2eb8bcf3eee512
fourWayz <157867069+fourWayz@users.noreply.github.com>
2024-07-27 09:44:28 +0000

    implement social login using privy

---
+++ client/providers/privyProviders.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+export default function Providers({children}: {children: React.ReactNode}) {
+  return (
+    <PrivyProvider
+      appId="cly62qv3b0d7x9hqd66oevmq7"
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
